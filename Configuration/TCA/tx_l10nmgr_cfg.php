0ee05655089f68db7fbbfe558b7d06bbd338d522
Sebastian Schneider <s.schneider@noeol.de>
2026-05-29 16:49:27 +0200

    [TASK] Allow configs to be stored on any page

--- Configuration/TCA/tx_l10nmgr_cfg.php
+++ Configuration/TCA/tx_l10nmgr_cfg.php
@@ -13,6 +13,9 @@ return [
         'cruser_id' => 'cruser_id',
         'default_sortby' => 'ORDER BY title',
         'iconfile' => 'EXT:l10nmgr/Resources/Public/Icons/icon_tx_l10nmgr_cfg.gif',
+        'security' => [
+            'ignorePageTypeRestriction' => true,
+        ],
     ],
     'columns' => [
         'title' => [

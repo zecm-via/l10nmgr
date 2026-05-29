e330049d9ba7bd32dccfd4819f347ee2533f3eb1
Sebastian Schneider <s.schneider@noeol.de>
2026-05-29 16:46:54 +0200

    [TASK] Disable module registration
    WIP!!!
    
    It needs to be clarified whether
    these modules still serve a purpose.

--- ext_tables.php
+++ ext_tables.php
@@ -9,36 +9,36 @@ defined('TYPO3') or die();
 /**
  * Registers a Backend Module
  */
-ExtensionManagementUtility::addModule(
-    'web',
-    'ConfigurationManager',
-    '',
-    '',
-    [
-        'routeTarget' => ConfigurationManager::class . '::mainAction',
-        'access'      => 'user,group',
-        'name'        => 'web_ConfigurationManager',
-        'icon'        => 'EXT:l10nmgr/Resources/Public/Icons/module-l10nmgr.svg',
-        'labels'      => 'LLL:EXT:l10nmgr/Resources/Private/Language/Modules/ConfigurationManager/locallang_mod.xlf',
-    ]
-);
+//ExtensionManagementUtility::addModule(
+//    'web',
+//    'ConfigurationManager',
+//    '',
+//    '',
+//    [
+//        'routeTarget' => ConfigurationManager::class . '::mainAction',
+//        'access'      => 'user,group',
+//        'name'        => 'web_ConfigurationManager',
+//        'icon'        => 'EXT:l10nmgr/Resources/Public/Icons/module-l10nmgr.svg',
+//        'labels'      => 'LLL:EXT:l10nmgr/Resources/Private/Language/Modules/ConfigurationManager/locallang_mod.xlf',
+//    ]
+//);
 
 /**
  * Registers a Backend Module
  */
-ExtensionManagementUtility::addModule(
-    'LocalizationManager',
-    '',
-    '',
-    '',
-    [
-        'routeTarget' => \Localizationteam\L10nmgr\Controller\LocalizationManager::class . '::mainAction',
-        'access'      => 'user,group',
-        'name'        => 'LocalizationManager',
-        'icon'        => 'EXT:l10nmgr/Resources/Public/Icons/module-l10nmgr.svg',
-        'labels'      => 'LLL:EXT:l10nmgr/Resources/Private/Language/Modules/ConfigurationManager/locallang_mod.xlf',
-    ]
-);
+//ExtensionManagementUtility::addModule(
+//    'LocalizationManager',
+//    '',
+//    '',
+//    '',
+//    [
+//        'routeTarget' => \Localizationteam\L10nmgr\Controller\LocalizationManager::class . '::mainAction',
+//        'access'      => 'user,group',
+//        'name'        => 'LocalizationManager',
+//        'icon'        => 'EXT:l10nmgr/Resources/Public/Icons/module-l10nmgr.svg',
+//        'labels'      => 'LLL:EXT:l10nmgr/Resources/Private/Language/Modules/ConfigurationManager/locallang_mod.xlf',
+//    ]
+//);
 
 /**
  * Registers a Backend Module
@@ -60,31 +60,19 @@ ExtensionManagementUtility::addModule(
 /**
  * Registers a Backend Module
  */
-ExtensionManagementUtility::addModule(
-    'LocalizationManager',
-    'TranslationTasks',
-    '',
-    '',
-    [
-        'routeTarget' => TranslationTasks::class . '::mainAction',
-        'access'      => 'user,group',
-        'name'        => 'LocalizationManager_TranslationTasks',
-        'icon'        => 'EXT:l10nmgr/Resources/Public/Icons/module-l10nmgr-tasks.svg',
-        'labels'      => 'LLL:EXT:l10nmgr/Resources/Private/Language/Modules/Module2/locallang_mod.xlf',
-    ]
-);
-
-// Add context sensitive help (csh) for the Scheduler tasks
-ExtensionManagementUtility::addLLrefForTCAdescr(
-    '_tasks_txl10nmgr',
-    'EXT:l10nmgr/Resources/Private/Language/Task/locallang_csh_tasks.xlf'
-);
-
-ExtensionManagementUtility::allowTableOnStandardPages('tx_l10nmgr_cfg');
-ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_l10nmgr_cfg',
-    'EXT:l10nmgr/Resources/Private/Language/locallang_csh_l10nmgr.xlf'
-);
+//ExtensionManagementUtility::addModule(
+//    'LocalizationManager',
+//    'TranslationTasks',
+//    '',
+//    '',
+//    [
+//        'routeTarget' => TranslationTasks::class . '::mainAction',
+//        'access'      => 'user,group',
+//        'name'        => 'LocalizationManager_TranslationTasks',
+//        'icon'        => 'EXT:l10nmgr/Resources/Public/Icons/module-l10nmgr-tasks.svg',
+//        'labels'      => 'LLL:EXT:l10nmgr/Resources/Private/Language/Modules/Module2/locallang_mod.xlf',
+//    ]
+//);
 
 // Example for disabling localization of specific fields in tables like tt_content
 // Add as many fields as you need
